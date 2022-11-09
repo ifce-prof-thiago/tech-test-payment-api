8a479a4b782228bfe7eeb43c21af2c079278f2de
Thiago Bandeira <thiago@lar.ifce.edu.br>
2022-11-09 15:49:52 -0300

    Iniciando o teste para nome obrigatório

--- core/src/test/java/br/com/pottencial/domain/entity/SalesmanTest.java
+++ core/src/test/java/br/com/pottencial/domain/entity/SalesmanTest.java
@@ -1,9 +1,9 @@
 package br.com.pottencial.domain.entity;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class SalesmanTest {
 
@@ -18,11 +18,11 @@ class SalesmanTest {
 
         // When
         final var actualSalesman = Salesman.of(
-                        expectedName,
-                        expectedCpf,
-                        expectedEmail,
-                        expectedPhone
-                );
+                expectedName,
+                expectedCpf,
+                expectedEmail,
+                expectedPhone
+        );
 
         // Then
         assertNotNull(actualSalesman);
@@ -33,5 +33,27 @@ class SalesmanTest {
         assertEquals(expectedPhone, actualSalesman.getPhone());
 
     }
+
+    @Test
+    void givenAnInvalidNullName_whenCallCreateSalesman_thenThrowDomainException() {
+        // Given
+        final var expectedErrorMessage = "'name' should not be null";
+
+        // When
+        final var actualException =
+                assertThrows(RuntimeException.class,
+                        () -> Salesman.of(
+                                null,
+                                "12345678900",
+                                "j@gmail.com",
+                                "(88) 99797 9990"
+                        )
+                );
+
+        // Then
+        assertEquals(expectedErrorMessage, actualException.getMessage());
+
+    }
+
 
 }
